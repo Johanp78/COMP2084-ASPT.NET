9626888109fed220f432ec03c2991512b05723c3
Johanp78 <156461599+Johanp78@users.noreply.github.com>
2024-10-09 17:31:25 -0400

    Category.cs creation

---
+++ VotingApplication/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VotingApplication.Models
+{
+    public class Category
+    {
+        //Primary key
+        
+        public int CategoryId {get; set;}
+
+        //Category Name
+        [Required(ErrorMessage = "Category Name is required")]
+        public string Name {get; set;}
+
+        public 
+    
+    }
+}
